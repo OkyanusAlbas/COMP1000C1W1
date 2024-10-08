d3c6a339eeb67fbe96b66c1c809d5b32f851f9db
OkyanusAlbas <albaokya@icloud.com>
2024-10-08 17:06:17 +0100

    Add files via upload

---
+++ COMP1000C1W1T1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+
+int main ()
+{
+int x, y;    // Int Variables
+int sum;    // Int Variable
+
+cout << "Type a num for x: "; // Output a text massage for user
+cin >> x; // Initialize the variable x
+
+cout <<"Type a num for y: "; // Output a text massage for user
+cin >> y; // Initialize the variable y
+
+sum = x + y; // Sum is equal to x add y
+cout << "Sum is: " << sum << endl; // Output Sum
+
+return 0; // terminates
+
+}
